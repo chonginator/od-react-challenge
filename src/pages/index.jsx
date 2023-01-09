932280a10dfbfe466d834b5311cbdfd2fe74d9c0
Ben Chong <benchong1999@yahoo.com.au>
2023-01-09 11:31:06 +1000

    Create Layout component and add to App

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,11 @@
+import Layout from "../components/Layout";
+
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <Layout>
+      <h1>Hello, World!</h1>
+    </Layout>
+  );
 };
 
 export default App;

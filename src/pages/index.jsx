e6c9dcb7a6a21cd20897a0f3c0828a2688e65ad4
Ben Chong <benchong1999@yahoo.com.au>
2023-01-09 14:19:36 +1000

    Add gallery page tab panels

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Tabs, Tab } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
 
-const App = () => {
+const Index = () => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
 
   const handleTabChange = (_, tabIndex) => {
@@ -11,11 +12,16 @@ const App = () => {
     <>
       <h1>Hello, World!</h1>
       <Tabs value={currentTabIndex} onChange={handleTabChange}>
-        <Tab label="Videos" />
-        <Tab label="Photos" />
+        <Tab value="1" label="Videos" />
+        <Tab value="2" label="Photos" />
       </Tabs>
+
+      <TabContext value={currentTabIndex}>
+        <TabPanel value="1">Panel 1</TabPanel>
+        <TabPanel value="2">Panel 2</TabPanel>
+      </TabContext>
     </>
   );
 };
 
-export default App;
+export default Index;

bd76cfd6aff733111cbfa63c7220cbb145b4b58b
Ben Chong <benchong1999@yahoo.com.au>
2023-01-09 14:40:21 +1000

    Fetch data from API on index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Tabs, Tab } from "@mui/material";
 import { TabContext, TabPanel } from "@mui/lab";
 
 const Index = () => {
   const [currentTabIndex, setCurrentTabIndex] = useState("0");
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
 
   const handleTabChange = (_, tabIndex) => {
     setCurrentTabIndex(tabIndex);
@@ -12,13 +21,13 @@ const Index = () => {
     <>
       <h1>Hello, World!</h1>
       <Tabs value={currentTabIndex} onChange={handleTabChange}>
-        <Tab value="1" label="Videos" />
-        <Tab value="2" label="Photos" />
+        <Tab value="0" label="Videos" />
+        <Tab value="1" label="Photos" />
       </Tabs>
 
       <TabContext value={currentTabIndex}>
-        <TabPanel value="1">Panel 1</TabPanel>
-        <TabPanel value="2">Panel 2</TabPanel>
+        <TabPanel value="0">Panel 1</TabPanel>
+        <TabPanel value="1">Panel 2</TabPanel>
       </TabContext>
     </>
   );

95374c9a46d0429e99f73a803815eae85faa93ea
Ben Chong <benchong1999@yahoo.com.au>
2023-01-09 13:58:23 +1000

    Create API route

---
+++ src/pages/api/index.js
@@ -0,0 +1,11 @@
+import data from "../../public/data.json";
+
+const handler = (req, res) => {
+  try {
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+export default handler;

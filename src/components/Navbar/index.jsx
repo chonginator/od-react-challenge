276cb2aff565b0d0aa9902eced8fec9b8edc96a0
Ben Chong <benchong1999@yahoo.com.au>
2023-01-10 15:23:40 +1000

    Add details to nav menu on details page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,19 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
-import { AppBar, Toolbar, IconButton, Drawer } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Box,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Menu, Favorite, ChatBubble } from "@mui/icons-material";
 import NavItems from "./NavItems";
 
 const Navbar = () => {
+  const router = useRouter();
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
   const handleDrawerOpen = () => {
@@ -14,6 +24,35 @@ const Navbar = () => {
     setIsDrawerOpen(false);
   };
 
+  const NavbarDetails = () => {
+    return (
+      <Box>
+        <Box>
+          <Typography component="h1">
+            Lost on the Road to the mountains
+          </Typography>
+        </Box>
+        <Box display="flex" alignItems="center">
+          <Box>
+            <Box display="flex">
+              <Typography>By: Alan Tiger</Typography>
+              <Typography>At: Paris, France</Typography>
+            </Box>
+            <Typography>On: March 31st, 2016</Typography>
+          </Box>
+          <Box>
+            <Button startIcon={<Favorite />} color="inherit">
+              4.5K
+            </Button>
+            <Button startIcon={<ChatBubble />} color="inherit">
+              1.1K
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    );
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -22,9 +61,12 @@ const Navbar = () => {
           aria-label="menu"
           onClick={handleDrawerOpen}
         >
-          <MenuIcon />
+          <Menu />
         </IconButton>
       </Toolbar>
+
+      {router.pathname === "/details" && <NavbarDetails />}
+
       <Drawer anchor="left" open={isDrawerOpen} onClose={handleDrawerClose}>
         <NavItems />
       </Drawer>

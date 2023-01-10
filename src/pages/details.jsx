2a4f5925eb41278b9b652c79e8aae8f9101a971c
Ben Chong <benchong1999@yahoo.com.au>
2023-01-10 13:28:57 +1000

    Add user cards to details page

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+
 const Details = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <>
       <h1>Details</h1>
+      {data?.users?.map((user, index) => {
+        return (
+          <Card key={index} sx={{ display: "flex" }}>
+            <CardMedia
+              image={`/images/${user.photoFilename}`}
+              alt={`${user.name}'s profile icon`}
+              sx={{ width: 100, height: 100 }}
+            />
+            <CardContent>
+              <Typography component="h2">{user.name}</Typography>
+              <Typography component="p">
+                These is very adorable. I am loving this. Sometimes this picture
+                just are beautiful!
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
     </>
   );
 };
